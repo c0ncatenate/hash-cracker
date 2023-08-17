41f318fdd0f9885e0b71a0f0388fd91ab8d9ccb2
NT <128177967+noto-21@users.noreply.github.com>
2023-08-16 23:51:55 -0400

    Optimizations
    Updated to incorporate more user-friendly design, as well as expanded features (eg. runtime timer)

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import hashlib
 import itertools
+import sys
 import threading
 import time
 from datetime import datetime
-import sys
+from timeit import default_timer
 from colorama import Fore, Back
 
 
-class style:
+class Style:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     END = '\033[0m'
@@ -15,40 +16,60 @@ class style:
 
 done = False
 
-# Headers
 
-print(Fore.RED + '░░░██╗░██╗  ░██╗░░██╗░█████╗░░██████╗██╗░░██╗' + Fore.BLUE + '  ░█████╗░██████╗░░█████╗░░█████╗░██╗░░██╗███████╗██████╗░')
-print(Fore.RED + '██████████╗  ██║░░██║██╔══██╗██╔════╝██║░░██║' + Fore.BLUE + '  ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗')
-print(Fore.RED + '╚═██╔═██╔═╝  ███████║███████║╚█████╗░███████║' + Fore.BLUE + '  ██║░░╚═╝██████╔╝███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝')
-print(Fore.RED + '██████████╗  ██╔══██║██╔══██║░╚═══██╗██╔══██║' + Fore.BLUE + '  ██║░░██╗██╔══██╗██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗')
-print(Fore.RED + '╚██╔═██╔══╝  ██║░░██║██║░░██║██████╔╝██║░░██║' + Fore.BLUE + '  ╚█████╔╝██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║')
-print(Fore.RED + '░╚═╝░╚═╝░░░  ╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝' + Fore.BLUE + '  ░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝' + Fore.RESET)
-print('')
-print(Fore.RED + '                                          SHA256 Hash' + Fore.MAGENTA + ' Cracker Written ' + Fore.BLUE + 'by' + ' concatenate' + Fore.RESET)
+def cracked(don):  # Cadenza
+    don = True
+    print(Fore.LIGHTWHITE_EX + '\r> ' + Style.BOLD + Fore.LIGHTMAGENTA_EX + '--Decryption complete!--' +
+          Fore.RESET + Style.END)
+    return don
+
+
+def print_end(crack, time1):  # Coda
+    time2 = default_timer()
+    print(Fore.LIGHTGREEN_EX + "\nINPUT".ljust(11, ' ') + Fore.LIGHTWHITE_EX + ': ' + Fore.LIGHTYELLOW_EX + Back.BLACK + sha256hash + Fore.RESET + Back.RESET)
+    if crack:
+        print(Fore.LIGHTRED_EX + "STATUS".ljust(10, ' ') + Fore.LIGHTWHITE_EX + ': ' + Fore.LIGHTRED_EX + Back.BLACK + "CRACK SUCCESS" + Back.RESET)
+        print(Fore.LIGHTBLUE_EX + "VERDICT".ljust(10, ' ') + Fore.LIGHTWHITE_EX + ': ' + Back.RED + Fore.LIGHTWHITE_EX + str(f"'{guess.decode('UTF-8')}' is your password.  Try something stronger!") + Back.RESET)
+        print(Fore.WHITE + "\nDecryption runtime: " + str(f"{time2 - time1:5.3f}s") + Fore.RESET)
+    else:
+        print(Fore.LIGHTRED_EX + "STATUS".ljust(10, ' ') + Fore.LIGHTWHITE_EX + ': ' + Fore.LIGHTGREEN_EX + Back.BLACK + "CRACK FAIL" + Back.RESET)
+        print(Fore.LIGHTBLUE_EX + "VERDICT".ljust(10, ' ') + Fore.LIGHTWHITE_EX + ': ' + Back.GREEN + Fore.LIGHTWHITE_EX + "I couldn't crack your hash, you have a strong password!" + Back.RESET)
+        print(Fore.WHITE + "\nDecryption runtime: " + str(f"{time2 - time1:5.3f}s") + Fore.RESET)
+
+
+# Headers
+print(Fore.LIGHTGREEN_EX + '░░░██╗░██╗' + Fore.LIGHTRED_EX + '  ░██╗░░██╗░█████╗░░██████╗██╗░░██╗' + Fore.LIGHTBLUE_EX + '  ░█████╗░██████╗░░█████╗░░█████╗░██╗░░██╗███████╗██████╗░')
+print(Fore.LIGHTGREEN_EX + '██████████╗' + Fore.LIGHTRED_EX + '  ██║░░██║██╔══██╗██╔════╝██║░░██║' + Fore.LIGHTBLUE_EX + '  ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗')
+print(Fore.LIGHTGREEN_EX + '╚═██╔═██╔═╝' + Fore.LIGHTRED_EX + '  ███████║███████║╚█████╗░███████║' + Fore.LIGHTBLUE_EX + '  ██║░░╚═╝██████╔╝███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝')
+print(Fore.LIGHTGREEN_EX + '██████████╗' + Fore.LIGHTRED_EX + '  ██╔══██║██╔══██║░╚═══██╗██╔══██║' + Fore.LIGHTBLUE_EX + '  ██║░░██╗██╔══██╗██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗')
+print(Fore.LIGHTGREEN_EX + '╚██╔═██╔══╝' + Fore.LIGHTRED_EX + '  ██║░░██║██║░░██║██████╔╝██║░░██║' + Fore.LIGHTBLUE_EX + '  ╚█████╔╝██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║')
+print(Fore.LIGHTGREEN_EX + '░╚═╝░╚═╝░░░' + Fore.LIGHTRED_EX + '  ╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝' + Fore.LIGHTBLUE_EX + '  ░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝' + Fore.RESET + '\n')
+print(Fore.GREEN + 'SHA256'.rjust(45, ' ') + Fore.RED + ' Hash' + Fore.BLUE + ' Cracker\n' + Fore.MAGENTA + 'Written by c0ncatenate'.rjust(56, ' ') + Fore.RESET)
+print(Fore.WHITE + 'For the purposes of decrypting encrypted text, via Secure Hash Algorithm 256'.rjust(88, '_').ljust(99, '_') + Fore.RESET)
 print('')
 
 # Get a hash input from the user
-sha256hash = input(Fore.LIGHTWHITE_EX + "[i] " + Fore.LIGHTBLUE_EX + "Put in your password hash here\n  > " + Fore.RESET)
+sha256hash = input(Fore.LIGHTWHITE_EX + '> ' + Fore.LIGHTYELLOW_EX + "Encrypted Password: " + Fore.RESET)
 print('')
-current_time = datetime.now().strftime("%H:%M:%S")
-print(Fore.LIGHTWHITE_EX + "[i] " + Fore.LIGHTMAGENTA_EX + "Cracking started: " + Fore.LIGHTWHITE_EX + current_time + '\n' + Fore.RESET)
+current_time = datetime.now().strftime("%H:%M:%S %m/%d/%Y")
+print(Fore.BLACK + Back.LIGHTWHITE_EX + "Decryption initiated @ " + current_time + Fore.RESET + Back.RESET + '\n')
+time_start = default_timer()
 
 
-# here is the animation
+# Animated loading
 def animate():
-    for c in itertools.cycle(['...', '..', '.']):
+    for c in itertools.cycle(['--', '<>', '[]', '<>']):
         if done:
             break
-        sys.stdout.write(style.BOLD + Fore.LIGHTRED_EX + '\rCracking' + c + Fore.RESET + style.END)
+        sys.stdout.write(Fore.LIGHTWHITE_EX + '\r> ' + Style.BOLD + Fore.LIGHTCYAN_EX + c + 'Decrypting' + c + Fore.RESET + Style.END)
         sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('\r')
+        time.sleep(0.2)
 
 
 t = threading.Thread(target=animate)
 t.start()
 
-wordlist = bytes(open("10-million-password-list-top-1000000.txt").read(), encoding='utf-8')
+wordlist = bytes(open("milwordlist.txt").read(), encoding='utf-8')
 
 # Hash the passwords in the wordlist
 for guess in wordlist.split():
@@ -57,20 +78,12 @@ for guess in wordlist.split():
     # Compare the hashes and give an output
     if hashed == sha256hash:
         time.sleep(1)
-        done = True
-        print(Fore.LIGHTWHITE_EX + "\n\nYour hash   :", Fore.LIGHTYELLOW_EX + sha256hash + Fore.RESET)
-        print(Fore.LIGHTWHITE_EX + "STATUS      : " + Fore.LIGHTGREEN_EX + "CRACKED")
-        print(Fore.LIGHTWHITE_EX + "GUESS       : " + Back.GREEN + Fore.LIGHTWHITE_EX + guess.decode(
-            'UTF-8') + Back.RESET)
-        print(Fore.LIGHTMAGENTA_EX + "\nCracking ended: " + Fore.LIGHTWHITE_EX + current_time + Fore.RESET)
+        done = cracked(done)
+        print_end(True, time_start)
         quit()
 
     # Output if the hashes don't match
     elif hashed != sha256hash:
         pass
-done = True
-print(Fore.LIGHTWHITE_EX + "\n\nYour hash   :", Fore.MAGENTA + sha256hash + Fore.RESET)
-print(Fore.LIGHTWHITE_EX + "STATUS      : " + Fore.LIGHTRED_EX + "UNCRACKED")
-print(
-    Fore.LIGHTWHITE_EX + "GUESS       : " + Back.RED + Fore.LIGHTWHITE_EX + "I couldn't crack your hash, you have a strong password! :)" + Back.RESET)
-print(Fore.LIGHTMAGENTA_EX + "\nCracking ended: " + Fore.LIGHTWHITE_EX + current_time + Fore.RESET)
+done = cracked(done)
+print_end(False, time_start)

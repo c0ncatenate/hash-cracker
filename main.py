7011a6f8eb07d57eefa0380c83c21a6dc7bbc13f
NT <128177967+noto-21@users.noreply.github.com>
2023-08-20 23:55:03 -0400

    Update main.py
    Removed unnecessary code

--- main.py
+++ main.py
@@ -19,20 +19,20 @@ done = False
 
 def cracked(don):  # Cadenza
     don = True
-    print(f"{Fore.LIGHTWHITE_EX}\r> {Style.BOLD}{Fore.LIGHTMAGENTA_EX}--Decryption complete!--{Fore.RESET}{Style.END}")
+    print(f"{Fore.LIGHTWHITE_EX}\r> {Style.BOLD}{Fore.LIGHTMAGENTA_EX}--Decryption complete!--{Style.END}")
     return don
 
 
 def print_end(crack, time1):  # Coda
     time2 = default_timer()
-    print(Fore.LIGHTGREEN_EX + "\nINPUT".ljust(11, ' ') + Fore.LIGHTWHITE_EX + ': ' + Fore.LIGHTYELLOW_EX + Back.BLACK + sha256hash + Fore.RESET + Back.RESET)
+    print(Fore.LIGHTGREEN_EX + "\nINPUT".ljust(11, ' ') + Fore.LIGHTWHITE_EX + ': ' + Fore.LIGHTYELLOW_EX + Back.BLACK + sha256hash + Back.RESET)
     if crack:
         print(Fore.LIGHTRED_EX + "STATUS".ljust(10, ' ') + Fore.LIGHTWHITE_EX + ': ' + Fore.LIGHTRED_EX + Back.BLACK + "CRACK SUCCESS" + Back.RESET)
         print(Fore.LIGHTBLUE_EX + "VERDICT".ljust(10, ' ') + Fore.LIGHTWHITE_EX + ': ' + Back.RED + Fore.LIGHTWHITE_EX + f"'{guess.decode('UTF-8')}' is your password.  Try something stronger!" + Back.RESET)
     else:
         print(Fore.LIGHTRED_EX + "STATUS".ljust(10, ' ') + Fore.LIGHTWHITE_EX + ': ' + Fore.LIGHTGREEN_EX + Back.BLACK + "CRACK FAIL" + Back.RESET)
         print(Fore.LIGHTBLUE_EX + "VERDICT".ljust(10, ' ') + Fore.LIGHTWHITE_EX + ': ' + Back.GREEN + Fore.LIGHTWHITE_EX + "I couldn't crack your hash, you have a strong password!" + Back.RESET)
-    print(f"{Fore.WHITE}\nDecryption runtime: {time2 - time1:5.3f}s{Fore.RESET}")
+    print(f"{Fore.WHITE}\nDecryption runtime: {time2 - time1:5.3f}s")
 
 
 # Headers
@@ -41,14 +41,14 @@ print(f'{Fore.LIGHTGREEN_EX}██████████╗{Fore.LIGHTRED_EX}�
 print(f'{Fore.LIGHTGREEN_EX}╚═██╔═██╔═╝{Fore.LIGHTRED_EX}  ███████║███████║╚█████╗░███████║{Fore.LIGHTBLUE_EX}  ██║░░╚═╝██████╔╝███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝')
 print(f'{Fore.LIGHTGREEN_EX}██████████╗{Fore.LIGHTRED_EX}  ██╔══██║██╔══██║░╚═══██╗██╔══██║{Fore.LIGHTBLUE_EX}  ██║░░██╗██╔══██╗██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██╗')
 print(f'{Fore.LIGHTGREEN_EX}╚██╔═██╔══╝{Fore.LIGHTRED_EX}  ██║░░██║██║░░██║██████╔╝██║░░██║{Fore.LIGHTBLUE_EX}  ╚█████╔╝██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║')
-print(f'{Fore.LIGHTGREEN_EX}░╚═╝░╚═╝░░░{Fore.LIGHTRED_EX}  ╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝{Fore.LIGHTBLUE_EX}  ░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝{Fore.RESET}\n')
-print(Fore.GREEN + 'SHA256'.rjust(45, ' ') + Fore.RED + ' Hash' + Fore.BLUE + ' Cracker\n' + Fore.MAGENTA + 'Written by: c0ncatenate'.rjust(60, ' ') + Fore.RESET)
-print(Fore.WHITE + 'For the purposes of decrypting encrypted text, via Secure Hash Algorithm 256'.rjust(88, '_').ljust(99, '_') + Fore.RESET, end='\n\n')
+print(f'{Fore.LIGHTGREEN_EX}░╚═╝░╚═╝░░░{Fore.LIGHTRED_EX}  ╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝{Fore.LIGHTBLUE_EX}  ░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝\n')
+print(Fore.GREEN + 'SHA256'.rjust(45, ' ') + Fore.RED + ' Hash' + Fore.BLUE + ' Cracker\n' + Fore.MAGENTA + 'Written by: c0ncatenate'.rjust(60, ' '))
+print(Fore.WHITE + 'For the purposes of decrypting encrypted text, via Secure Hash Algorithm 256'.rjust(88, '_').ljust(99, '_'), end='\n\n')
 
 # Get a hash input from the user
-sha256hash = input(f'{Fore.LIGHTWHITE_EX}> {Fore.LIGHTYELLOW_EX}Encrypted Password: {Fore.RESET}')
+sha256hash = input(f'{Fore.LIGHTWHITE_EX}> {Fore.LIGHTYELLOW_EX}Encrypted Password: ')
 current_time = datetime.now().strftime("%H:%M:%S %m/%d/%Y")
-print(f"\n{Fore.BLACK}{Back.LIGHTWHITE_EX}Decryption initiated @ {current_time}{Fore.RESET}{Back.RESET}\n")
+print(f"\n{Fore.BLACK}{Back.LIGHTWHITE_EX}Decryption initiated @ {current_time}{Back.RESET}\n")
 time_start = default_timer()
 
 
@@ -57,7 +57,7 @@ def animate():
     for c in itertools.cycle(['--', '<>', '[]', '<>']):
         if done:
             break
-        sys.stdout.write(f'{Fore.LIGHTWHITE_EX}\r> {Style.BOLD}{Fore.LIGHTCYAN_EX}{c}Decrypting{c}{Fore.RESET}{Style.END}')
+        sys.stdout.write(f'{Fore.LIGHTWHITE_EX}\r> {Style.BOLD}{Fore.LIGHTCYAN_EX}{c}Decrypting{c}{Style.END}')
         sys.stdout.flush()
         time.sleep(0.2)
 
